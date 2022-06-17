419ad8980b2463bcccc70e578775ee39041a9436
unknown <corentin.malbrun1@gmail.com>
2022-06-17 10:18:26 +0200

    media fini

--- app.js
+++ app.js
@@ -10,3 +10,21 @@ menuHamburger.addEventListener("click",() => {
 window.onscroll =() => {
     navbar.classList.remove("active-menu");
 }
+
+const tabs = [...document.querySelectorAll(".tab")]
+
+tabs.forEach(tab => tab.addEventListener("click", tabsAnimation))
+
+function tabsAnimation(e){
+    const tabContents = [...document.querySelectorAll(".tab-content")]
+
+    const indexRemove = tabs.findIndex(tab => tab.classList.contains("active-tab"))
+
+    tabs[indexRemove].classList.remove("active-tab");
+    tabContents[indexRemove].classList.remove("active-tab-content");
+
+    const indexToShow = tabs.indexOf(e.target)
+
+    tabs[indexToShow].classList.add("active-tab");
+    tabContents[indexToShow].classList.add("active-tab-content");
+}
